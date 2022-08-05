6d51e7d2c5eb838e7cb3b1fc855a084ea0ff5ef5
Ondrej Fabry <ondrej.fabry@pantheon.tech>
2022-08-05 14:36:13 +0200

    Fix goreleaser and log replace

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -7,12 +7,12 @@ import (
 )
 
 func TraceElapsed(logger logrus.Ext1FieldLogger, name string) func() {
-	logger.WithFields(nil).WithCaller(1).
-		Tracef("begin %v", name)
+	logger.WithFields(nil). //WithCaller(1).
+				Tracef("begin %v", name)
 	t := time.Now()
 	return func() {
 		elapsed := time.Since(t)
-		logger.WithFields(nil).WithCaller(1).
-			Tracef("done %v (took %v)", name, elapsed)
+		logger.WithFields(nil). //WithCaller(1).
+					Tracef("done %v (took %v)", name, elapsed)
 	}
 }
